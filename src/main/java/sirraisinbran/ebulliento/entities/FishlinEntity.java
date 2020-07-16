de1bc37f9a2629b5d2cd23cbcb12dd1160c29a04
SirRaisinBran <cyanstoves@gmail.com>
2020-07-16 11:50:58 -0500

    Update FishlinEntity.java

--- src/main/java/sirraisinbran/ebulliento/entities/FishlinEntity.java
+++ src/main/java/sirraisinbran/ebulliento/entities/FishlinEntity.java
@@ -5,31 +5,42 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.PanicGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
-public class FishlinEntity extends AgeableEntity {
+public class FishlinEntity extends AnimalEntity {
 	
-	public FishlinEntity(EntityType<? extends AgeableEntity> type, World worldIn) {
+	public FishlinEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
 		super(type, worldIn);
 	}
-	
-	BlockPos blockpos = this.func_233580_cy_();
 
 	@Override
 	protected void registerGoals() {
-		this.goalSelector.addGoal(1, new SwimGoal(this));
-		this.goalSelector.addGoal(2, new RandomWalkingGoal(this, 1.2d));
+		this.goalSelector.addGoal(0, new SwimGoal(this));
+	    this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
+		this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.5D, true));
+		this.goalSelector.addGoal(3, new LookAtGoal(this, PlayerEntity.class, 1.0F));
+	    this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
 	}
 	
 	public static AttributeModifierMap.MutableAttribute func_234551_eU_() {
-	    return MobEntity.func_233666_p_().func_233815_a_(Attributes.field_233821_d_, 0.5D).func_233815_a_(Attributes.field_233819_b_, 48.0D);
+	    return MobEntity.func_233666_p_().func_233815_a_(Attributes.field_233821_d_, 0.5D).func_233815_a_(Attributes.field_233819_b_, 48.0D).func_233815_a_(Attributes.field_233818_a_, 15.0D);
 	}
 
 	@Override
 	public FishlinEntity createChild(final AgeableEntity parent) {
 		return (FishlinEntity) getType().create(this.world);
+	}
+	
+	@Override
+	public boolean canDespawn(double distanceToClosestPlayer)
+	{
+		return false;
 	}
 }
